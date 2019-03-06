bdec84f7d6c72e21e95ccfe0cb7555239b0d4618
joseantonio15 <47762123+joseantonio15@users.noreply.github.com>
2019-03-06 17:42:21 -0600

    hola
    insertar h1

---
+++ curriculo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>currículo</title>
+</head>
+<body>
+ <div id=apartado class="apart">
+ <img  src="imagen/tony.jpg">
+ 
+<ul>
+	<br><br>Curriculu Vitae 
+	<br><br>Acerca de Mi 
+	<br><br>Experiencia 
+	<br><br>Educación 
+	<br><br>Habilidades 
+    <br><br>Reconocimientos 
+	<br><br>contacto
+	</ul>
+</div>
+<center><div class="caja" id="caja1">
+	<h1>hola</h1>
+</div></center>
+</body>
+</html>
